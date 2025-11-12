ba2f0495d3a8e831d0da2d33473d947fc3975d58
thepandeyakash <akashbspandey01@gmail.com>
2025-11-12 05:48:07 +0530

    Time: 19 ms (64.32%), Space: 43.4 MB (52.05%) - LeetHub

---
+++ 0474-ones-and-zeroes/0474-ones-and-zeroes.java
@@ -0,0 +1,21 @@
+class Solution {
+    public int findMaxForm(String[] strs, int m, int n) {
+        int dp[][] = new int[m+1][n+1];
+
+        for(String s: strs){
+            int zeroes = 0, ones = 0;
+            for(char ch : s.toCharArray()){
+                if(ch == '0') zeroes++;
+                else ones++;
+            }
+
+            for(int i = m; i>=zeroes; i--){
+                for(int j = n; j>= ones; j--){
+                    dp[i][j] = Math.max(dp[i][j], 1+dp[i-zeroes][j-ones]);
+                }
+            }
+        }
+
+        return dp[m][n];
+    }
+}
