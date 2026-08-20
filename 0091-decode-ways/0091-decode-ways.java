eaf5bc3845ffc63cd3aec1f081a08d6845d0ec46
thepandeyakash <akashbspandey01@gmail.com>
2026-08-20 18:01:41 +0530

    Time: 1 ms (79.97%), Space: 43.2 MB (33.72%) - LeetHub

---
+++ 0091-decode-ways/0091-decode-ways.java
@@ -0,0 +1,23 @@
+class Solution {
+    public int numDecodings(String s) {
+        int n = s.length();
+        int [] dp = new int[n+1];
+        dp[0] = 1;
+
+        if(s.charAt(0) != '0'){
+            dp[1] = 1;
+        }
+
+        for(int i = 2; i<=n; i++ ){
+            if(s.charAt(i-1) != '0'){
+                dp[i] += dp[i-1];
+            }
+
+            int num = Integer.parseInt(s.substring(i-2, i));
+            if(num >= 10 && num<=26){
+                dp[i] += dp[i-2];
+            }
+        }
+        return dp[n];
+    }
+}
